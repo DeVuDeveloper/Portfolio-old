841505038c090a25077f8509c286432a7906035e
Olawale <bolawale81@gmail.com>
2021-12-13 12:39:58 +0100

    Added html and script..almost done-Olawale

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const openMenu = document.querySelector('#open');
-const closeMenu = document.querySelector('.close');
+const closeMenu = document.querySelectorAll('.close');
 const slideMenu =document.querySelector('#slidebar');
 
 openMenu.addEventListener('click', function() {
     slideMenu.classList.add('show');
 });
 
-closeMenu.addEventListener('click', function() {
-  slideMenu.classList.remove('show');
+// closeMenu.addEventListener('click', function() {
+//   slideMenu.classList.remove('show');
+// });
+closeMenu.forEach(element => {
+  element (slideMenu.classList.remove('show'));
 });
