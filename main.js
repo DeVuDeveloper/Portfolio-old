55c13eabdc7b37c1f6d073b9124065f4b9a9b0b2
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-13 15:14:25 +0100

    fix

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-const openMenu = document.querySelector('#open');
-const hideMenu = document.querySelector('#close');
-const sideMenu = document.querySelector('#sidebar');
 
-openMenu.addEventListener('click', function() {
-  sideMenu.classList.add('active')
-});
-
-
-hideMenu.addEventListener('click', function() {
-    sideMenu.classList.remove('active')
-});
