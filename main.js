761efbe42c647516bc00987cbe77108164c29eaf
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-13 15:59:51 +0100

    fixing linters

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const openMenu = document.querySelector('#open');
 const closeMenu = document.querySelectorAll('.close');
-const slideMenu =document.querySelector('#slidebar');
+const slideMenu = document.querySelector('#slidebar');
 
-openMenu.addEventListener('click', function() {
-    slideMenu.classList.add('show');
+openMenu.addEventListener('click', function () {
+  slideMenu.classList.add('show');
 });
 
-closeMenu.forEach(element => {
-  element.addEventListener('click', () => slideMenu.classList.remove("show"));
+closeMenu.forEach((element) => {
+  element.addEventListener('click', () => slideMenu.classList.remove('show'));
 });
