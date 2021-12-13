74d2020238ab9c6d6851bb15fde19c4c753b766b
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-13 17:27:20 +0100

    done

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const openMenu = document.querySelector('#open');
 const closeMenu = document.querySelectorAll('.close');
 const slideMenu = document.querySelector('#slidebar');
 
-openMenu.addEventListener = ('click', () => {
-  slideMenu.classList.add('show');
+openMenu.addEventListener('click', () => {
+  slideMenu.classList.add('show')
 });
 
 closeMenu.forEach((element) => {
