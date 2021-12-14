2710f1a03cddae621bb6b1643772e2fec60504b2
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-14 14:38:50 +0100

    done with html and css both versions and pop-up js made

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ openMenu.addEventListener('click', () => {
 closeMenu.forEach((element) => {
   element.addEventListener('click', () => slideMenu.classList.remove('show'));
 });
+
+
+
+const openPop = document.querySelectorAll('.open-pop');
+const closePop = document.querySelector('.pop-x');
+const popUp = document.querySelector('.pop-up');
+
+openPop.forEach((element) => {
+  element.addEventListener('click', () => popUp.classList.add('pop'));
+});
+
+closePop.addEventListener('click', () => {
+  popUp.classList.remove('pop');
+});
+
