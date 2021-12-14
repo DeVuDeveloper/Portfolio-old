1580dda474c8fd9e822f3ed9031326c94e1790e0
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-14 14:59:07 +0100

    working on object

--- main.js
+++ main.js
@@ -24,3 +24,74 @@ closePop.addEventListener('click', () => {
   popUp.classList.remove('pop');
 });
 
+const myProjects = [
+{
+  project: 1,
+  title: 'Multi Post Stories',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+{
+  project: 1,
+  title: 'Multi Post Stories',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+{
+  project: 1,
+  title: 'Multi Post Stories',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+{
+  project: 1,
+  title: 'Profesional Art Printing Data More',
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+{
+  project: 1,
+  title: 'Multi Post Stories',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+{
+  project: 2,
+  title: 'Multi Post Stories',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+{
+  project: 1,
+  title: 'Multi Post Stories',
+  description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard.",
+  img: 'img/img/section2.png',
+  technologies: ['css', 'html', 'bootstrap', 'ruby'],
+  live: 'url(https://vudej.github.io/Responsive-Web-Design/)',
+  source: 'url(https://github.com/)',
+
+},
+];
