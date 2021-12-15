e60819a35fc22255274c5d7221dbd358287c6ab1
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-15 15:29:29 +0100

    made js files for all projects separately

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,16 +1,3 @@
-const openMenu = document.querySelector('#open');
-const closeMenu = document.querySelectorAll('.close');
-const slideMenu = document.querySelector('#slidebar');
-
-openMenu.addEventListener('click', () => {
-  slideMenu.classList.add('show');
-});
-
-closeMenu.forEach((element) => {
-  element.addEventListener('click', () => slideMenu.classList.remove('show'));
-});
-
-
 
 const openPop = document.querySelectorAll('.open-pop');
 const closePop = document.querySelector('.pop-x');
