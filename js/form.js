572d42704104d8d4991e2db018579b48cdbfb68d
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-15 20:19:04 +0100

    fix linter errors

--- js/form.js
+++ js/form.js
@@ -1,16 +1,15 @@
-const form = document.getElementById('form')
-form.addEventListener('submit', e => {
-  const email = document.getElementById('e-mail').value
-  const error = document.getElementById('error')
-  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-  if (email == email.toLowerCase() && email.match(pattern)) {
-    error.innerHTML = 'Your Email Address is Valid. Form is sent.'
-    error.style.color = '#00ff00'
-    form.submit()
+const form = document.getElementById('form');
+form.addEventListener('submit', (e) => {
+  const email = document.getElementById('e-mail').value;
+  const error = document.getElementById('error');
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (email === email.toLowerCase() && email.match(pattern)) {
+    error.innerHTML = 'Your Email Address is Valid. Form is sent.';
+    error.style.color = '#00ff00';
+    form.submit();
 } else {
     error.innerHTML = 'Please use lowercase!. Form is not sent'
-    error.style.color = '#ff0000'
-    e.preventDefault()
-}
+    error.style.color = '#ff0000';
+    e.preventDefault();
+  }
 });
-
