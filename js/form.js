c4062e20b2394f97c39de2c9675723ef496c08d7
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-15 19:53:57 +0100

    finished email validation

--- js/form.js
+++ js/form.js
@@ -1,4 +1,16 @@
 const form = document.getElementById('form')
-const email= document.getElementById('e-mail');
-const error = document.getElementById('error');
+form.addEventListener('submit', e => {
+  const email = document.getElementById('e-mail').value
+  const error = document.getElementById('error')
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+  if (email == email.toLowerCase() && email.match(pattern)) {
+    error.innerHTML = 'Your Email Address is Valid. Form is sent.'
+    error.style.color = '#00ff00'
+    form.submit()
+} else {
+    error.innerHTML = 'Please use lowercase!. Form is not sent'
+    error.style.color = '#ff0000'
+    e.preventDefault()
+}
+});
 
