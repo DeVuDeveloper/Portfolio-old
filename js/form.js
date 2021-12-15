9edd89d295815a09d221db6970e6160cbe212453
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-15 20:50:43 +0100

    linter

--- js/form.js
+++ js/form.js
@@ -4,12 +4,12 @@ form.addEventListener('submit', (e) => {
   const error = document.getElementById('error');
   const pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
   if (email === email.toLowerCase() && email.match(pattern)) {
-  error.innerHTML = 'Your Email Address is Valid. Form is sent.';
-  error.style.color = '#00ff00';
-  form.submit();
+    error.innerHTML = 'Your Email Address is Valid. Form is sent.';
+    error.style.color = '#00ff00';
+    form.submit();
   } else {
-  error.innerHTML = 'Please use lowercase!. Form is not sent';
-  error.style.color = '#ff0000';
-  e.preventDefault();
+    error.innerHTML = 'Please use lowercase!. Form is not sent';
+    error.style.color = '#ff0000';
+    e.preventDefault();
   }
 });
