b5f1cb224059be5b733a03557e5527f303d1a367
VuDej <93735137+VuDej@users.noreply.github.com>
2021-12-28 15:29:29 +0100

    fixing for mobile version

---
+++ js/form.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (element) => {
+  const email = document.getElementById('e-mail').value;
+  const error = document.getElementById('error');
+  const pattern = /^[^]+@[^]+\.[a-z]{2,3}$/;
+  if (email === email.toLowerCase() && email.match(pattern)) {
+    error.innerHTML = 'Your Email Address is Valid. Form is sent.';
+    error.style.color = '#00ff00';
+    form.submit();
+  } else {
+    error.innerHTML = 'Please use lowercase!. Form is not sent';
+    error.style.color = '#ff0000';
+    element.preventDefault();
+  }
+});
+
+const textArea = document.getElementById('user-comments');
+const lettersCounter = document.getElementById('counter');
+
+textArea.addEventListener('input', (event) => {
+  const maxLettersNumber = event.target.getAttribute('maxlength');
+  const currentLettersNumber = event.target.value.length;
+  lettersCounter.innerHTML = `${currentLettersNumber}/${maxLettersNumber}`;
+});
